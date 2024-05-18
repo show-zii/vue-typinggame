b525f92367f99a4e6a39da6c801f0dac2b8e6caa
Shoji.T <107194204+show-zii@users.noreply.github.com>
2024-05-18 18:28:09 +0900

    環境構築

---
+++ app/types/shims-vue.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* prettier-ignore */
+// @ts-nocheck
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
