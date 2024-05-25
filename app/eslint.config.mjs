c1fdd89863cd49838bf3c3ca842dbaf9f973ce2d
Shoji.T <107194204+show-zii@users.noreply.github.com>
2024-05-25 14:34:34 +0000

    ESLint実行時にunexpected Tokenが発生していたのを解消

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -2,11 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-
+import vueParser from "vue-eslint-parser";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { 
+    files: ["*.vue", "**/*.vue"],
+    languageOptions: { 
+      globals: globals.browser,
+      parser: vueParser,
+      parserOptions: { parser: tseslint.parser, sourceType: "module" },
+    } 
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  eslintConfigPrettier,
 ];
